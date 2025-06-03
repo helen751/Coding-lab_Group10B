af102bf257c16c49b56a6197be1e07376936523f
Umutoni123-aa <u.nada@alustudent.com>
2025-06-03 19:07:13 +0200

    analyze_temperature

--- hospital_data/reports/analyze_logs.sh
+++ hospital_data/reports/analyze_logs.sh
@@ -25,7 +25,32 @@ analyze_heart_rate() {
 }
 
 
-# analyze_temperature() {}
+
+analyze_temperature() {
+    file_name="hospital_data/active_logs/temperature.log"
+
+    # Check if the file exists
+    if [ ! -f "$file_name" ]; then
+        echo "File not found: $file_name"
+        exit 1
+    fi
+       echo "Analysing Temperature Sensors..."
+    # Check the devices we have 
+     devices=$(awk '{print $3}' "$file_name" | sort | uniq)
+
+    for device in $devices; do
+        count_number_of_occurance=$(awk -v device="$device" '$3 == device {count++} END {print count}' "$file_name")
+        echo "$device appears $count_number_of_occurance times in the temperature.log file"
+        done
+
+        first_timestamp=$(awk -v device="$device" 'device == $3 {print $1, $2 }' "$file_name" | head -n 1)
+        echo "$device first monitored on $first_timestamp"
+
+        last_timestamp=$(awk -v device="$device" 'device == $3 {print $1, $2 }' "$file_name" | tail -n 1)
+        echo "$device last monitored on $last_timestamp"
+        echo 
+    done
+}
 # analyze_water_usage() {}
 
 
